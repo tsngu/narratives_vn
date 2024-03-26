f6aa29d533c4c438ad9971bb040490a9e417c103
tsngu <tifannys.nguyen@gmail.com>
2024-03-26 11:36:11 +0100

    ajout de l'outil d'analyse syntaxique

---
+++ scripts/vncorenlp_script.py
@@ -0,0 +1,12 @@
+import py_vncorenlp
+
+# Automatically download VnCoreNLP components from the original repository
+# and save them in some local working folder
+#py_vncorenlp.download_model(save_dir='../vncorenlp/')
+
+# Load VnCoreNLP from the local working folder that contains both `VnCoreNLP-1.2.jar` and `models` 
+model = py_vncorenlp.VnCoreNLP(save_dir='../vncorenlp/')
+# Equivalent to: model = py_vncorenlp.VnCoreNLP(annotators=["wseg", "pos", "ner", "parse"], save_dir='/absolute/path/to/vncorenlp')
+
+# Annotate a raw corpus
+model.annotate_file(input_file="../output/btt/btt_url_1.txt", output_file="../output/vncorenlp/")
