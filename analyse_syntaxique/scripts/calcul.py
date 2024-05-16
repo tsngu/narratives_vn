868095ada5a6b43ee4d4fb075d0adc1ba4acb569
tsngu <tifannys.nguyen@gmail.com>
2024-05-16 18:38:32 +0200

    maj calcul

--- analyse_syntaxique/scripts/calcul.py
+++ analyse_syntaxique/scripts/calcul.py
@@ -1,36 +1,26 @@
 import csv
-from collections import defaultdict
 
-
-fichier = 'resultats_suj_cod.csv' 
-
-def colonne(file, n, sep=',') : 
-    with open(file, "r") as f: 
-        r = csv.reader(f, delimiter = sep)
+def colonne(file, n, sep=','):
+    with open(file, "r") as f:
+        r = csv.reader(f, delimiter=sep)
         rlignes = list(zip(*r))
-        if (n <len(rlignes)) and (n>= -len(rlignes)):  
-            return rlignes [n]
-        else : 
-            return[]
-        
+        if (-len(rlignes) <= n < len(rlignes)):
+            return rlignes[n]
+        else:
+            return []
 
 def wordListToFreqDict(liste_mots):
-    freqs_mots = [liste_mots.count(mot) for mot in liste_mots]
-    return dict(list(zip(liste_mots,freqs_mots)))
-    
+    freqs_mots = {mot: liste_mots.count(mot) for mot in liste_mots}
+    return freqs_mots
+
 def sortFreqDict(freqs_mots):
-    aux = [(freqs_mots[mot], mot) for mot in freqs_mots]
-    aux.sort()
-    aux.reverse()
-    return aux
+    return sorted(freqs_mots.items(), key=lambda x: x[1], reverse=True)[:100]
 
-if __name__ == "__main__": 
-    c = colonne(fichier, 1, sep=",")
-    print(type(c)) 
+if __name__ == "__main__":
+    fichier = 'resultats_suj_cod.csv'
+    c = colonne(fichier, 1)
+    print("Type de la colonne :", type(c))
     res = wordListToFreqDict(c)
-    print(res)
-    frequences_ordre = 
-
-            
-    
-
+    print("Dictionnaire de fréquence des mots :", res)
+    frequences_ordre = sortFreqDict(res)
+    print("Les 100 premières fréquences des mots ordonnées :", frequences_ordre)
